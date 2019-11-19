a284d605c01ef362d2a4df6a19c5f57e564d37e1
Janne Kurkinen <janne.kurkinen@solita.fi>
2019-11-19 13:06:28 +0200

    createComponent bug reproduction with Router

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,14 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api';
+
+export default createComponent({
+  name: 'AboutView',
+  setup() {
+    return {};
+  },
+});
+</script>
